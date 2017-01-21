721cc2eb5c308035d8e2bb874b28fad3d8b9cd2f
Eduardo Romo <eduromogo@outlook.es>
2017-01-21 15:41:01 +0100

    configured for upload images with dropzone

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 'use strict';
 
 console.log('hi');
+
+
+$(function() {
+  // Now that the DOM is fully loaded, create the dropzone, and setup the
+  // event listeners
+  var myDropzone = new Dropzone("#my-dropzone", { method: 'POST', url: "http://c292aebc.ngrok.io/test"});
+  myDropzone.on("success", function(file, res) {
+  	console.log(file);
+  	console.log('uploaded');
+  });
+});
